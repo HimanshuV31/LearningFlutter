83a76a9af09362af7ec756ec55093a304a966e76
Himanshu Vaishnav <himanshudv31@gmail.com>
2025-09-24 22:40:57 +0530

    240925 22:41 API Keys updated due to leak, Updated gitignore

--- lib/constants/api_keys.dart
+++ lib/constants/api_keys.dart
@@ -1,5 +1,5 @@
 String GeminiAPIKey()  {
-  return "AIzaSyDRdjymzd1lUGtUd-yCK0oBGSCLqZAUmoY";
+  return "AIzaSyDT_UllaiNaU2XP_anedufxhFQoMz-Ou-s";
 }
 String OpenAIAPIKey() {
   return "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";
