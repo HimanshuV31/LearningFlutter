2aec8b4bab70ce7d315e3342624848d2f810657e
Himanshu Vaishnav <82385365+HimanshuV31@users.noreply.github.com>
2025-09-24 22:39:45 +0530

    Delete lib/constants/api_keys.dart

--- lib/constants/api_keys.dart
+++
@@ -1,6 +0,0 @@
-String GeminiAPIKey()  {
-  return "AIzaSyDRdjymzd1lUGtUd-yCK0oBGSCLqZAUmoY";
-}
-String OpenAIAPIKey() {
-  return "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";
-}
